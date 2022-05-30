f150def4e6ed70df3dd2a77bdef3ad16b9e67673
Eliger Roa Rubio <eliger13@gmail.com>
2022-05-30 13:02:32 -0300

    Añadir clase VistaTransportePublico

---
+++ LabTransporte/LabTransporte/VistaTransportePublico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTransporte
+{
+    internal class VistaTransportePublico
+    {
+        [System.ComponentModel.DisplayName("Transporte")]
+        public string tipoTransporte { get; set; }
+
+        [System.ComponentModel.DisplayName("Numero")]
+        public int nroTransporte { get; set; }
+
+        [System.ComponentModel.DisplayName("Pasajeros")]
+        public int pasajeros { get; set; }
+
+        public VistaTransportePublico(string tipoTransporte, int nroTransporte, int pasajeros)
+        {
+            this.tipoTransporte = tipoTransporte;
+            this.nroTransporte = nroTransporte;
+            this.pasajeros = pasajeros;
+        }
+    }
+}
